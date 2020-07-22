197a3aeafb8f41f1f4a61b4fef52c2ddf3ae0ae2
Guillim <guigloo@msn.com>
2020-07-22 11:39:41 +0200

    adding purge of CSS for ligth app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    // enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
