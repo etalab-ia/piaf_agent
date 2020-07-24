d323fb6b642b13a38dfff700813c78f5c30f3f91
Guillim <guigloo@msn.com>
2020-07-24 11:49:24 +0200

    typo

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <router-link :to="{ name: 'Home' }">
     <img alt="Piaf logo" src="../assets/logopiaf.png" style="max-height:150px;max-width:150px;" class="mx-auto"></router-link>
-    <h1 class="m-0 text-5xl"> <span class="text-red-600">Piaf</span> & <span style="color:#47abd9">Service-publique.fr</span></h1>
+    <h1 class="m-0 text-5xl"> <span class="text-red-600">Piaf</span> & <span style="color:#47abd9">Service-public.fr</span></h1>
     <p class="text-xl mb-8">Une autre façon de trouver une information</p>
   </div>
 </template>
