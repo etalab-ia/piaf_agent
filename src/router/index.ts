cdc4c1b0727434529126d61eb4f0a74e8171fb3f
Guillim <guigloo@msn.com>
2020-07-24 07:23:47 +0200

    fixing blank page

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    name: 'catchAll',
+    component: Home
   }
 ]
 
