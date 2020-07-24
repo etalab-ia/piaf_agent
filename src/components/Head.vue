555bcf4d95046b609d90bbf6f5bb073e9ea37fa7
Guillim <guigloo@msn.com>
2020-07-24 10:29:30 +0200

    fix link that was without base_URL since <a href> aren't as good as <router-link :to

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <a href="/">
-    <img alt="Piaf logo" src="../assets/logopiaf.png" style="max-height:150px;max-width:150px;" class="mx-auto"></a>
+    <router-link :to="{ name: 'Home' }">
+    <img alt="Piaf logo" src="../assets/logopiaf.png" style="max-height:150px;max-width:150px;" class="mx-auto"></router-link>
     <h1 class="m-0 text-5xl"> <span class="text-red-600">Piaf</span> & <span style="color:#47abd9">Service-publique.fr</span></h1>
     <p class="text-xl mb-8">Une autre façon de trouver une information</p>
   </div>
