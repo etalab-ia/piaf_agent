517d7e0d65e657ed2cb05db542fa7d887869539e
Guillim <guigloo@msn.com>
2020-09-14 11:13:19 +0200

    update CSS according to UX recommendation

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="mb-2">
+    <div class="mb-2 mt-6">
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-screen-md w-11/12" id="question" type="text" placeholder="Poser votre question ici"
       v-on:keyup.enter="onClick" v-model="newQuestion">
     </div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" v-on:click="onClick">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mt-4" v-on:click="onClick">
       Chercher
     </button>
-    <div class="text-center text-gray-700 mt-8">
+    <div class="text-center text-gray-600 mt-10 italic">
       <p>ex: "Quel est le coût d'un passeport ?"</p>
     </div>
   </div>
