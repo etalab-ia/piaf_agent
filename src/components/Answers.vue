34645a2d68e9fa2493de93fa42d2ccca875f017a
Guillim <guillim@users.noreply.github.com>
2021-06-02 00:33:07 +0200

    Update Answers.vue
    to fit with miachel new typology

--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -33,7 +33,7 @@ export default Vue.extend({
   name: 'Answers',
   data: () => ({
     ready: false,
-    useFilters: (process.env.VUE_APP_USE_FILTERS == 'true')
+    useFilters: Boolean(process.env.VUE_APP_USE_FILTERS)
   }),
   computed: {
     ...mapState([
