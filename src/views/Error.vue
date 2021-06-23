874edeed1751ad5cff95d163879c092336e3ae7a
Guillim <guigloo@msn.com>
2021-06-23 15:47:41 +0200

    typo

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -3,7 +3,7 @@
     <Head/>
     <div class="mt-10">
       <div>
-        Il y a eu un erreur, désolé pour ça ! <br><br>
+        Il y a eu une erreur, désolé pour ça ! <br><br>
         <router-link class="rounded bg-blue-700 text-white hover:bg-blue-800 p-1" :to="{ name: 'Home' }">Poser une nouvelle question</router-link>
       </div>
     </div>
