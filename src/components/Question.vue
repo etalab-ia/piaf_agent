54e7cd9e0ab4389bc35d34475133cca0cb78d796
Guillim <guillim@users.noreply.github.com>
2021-06-24 10:36:46 +0200

    Ratings full stars (#37)
    * we change the behaviour of the stars when highlighting : only round rates are now possible.
    
    * small css fix for error handling

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="mb-2 mt-6 fr-search-bar justify-center">
+    <div class="mb-1 mt-6 fr-search-bar justify-center">
       <input class="fr-input w-full max-w-screen-md w-11/12" id="question" type="text" placeholder="Poser votre question ici"
       v-on:keyup.enter="onClick" v-model="newQuestion">
     </div>
-    <div class="mx-2">
-      <p v-if="error" id="text-input-error-desc-error" class="fr-error-text">
+    <div class="mx-2 mt-0 mb-2 flex justify-center">
+      <p v-if="error" id="text-input-error-desc-error" class="fr-error-text mt-0">
         {{error}}
       </p>
     </div>
