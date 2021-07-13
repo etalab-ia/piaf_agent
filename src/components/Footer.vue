c3ce8caecd0075205116311fa50ae65695cbd544
Guillim <guigloo@msn.com>
2021-07-13 15:48:17 +0200

    styling of contact us message

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="home">
+  <div class="home mt-10">
     <a :href="'mailto:piaf@data.gouv.fr'">
-      <header role="banner" class="fr-header">
+      <header role="banner">
         <div class="fr-header__body">
             <div class="fr-container">
                 <div class="fr-header__body-row justify-center">
                     <div class="fr-header__brand fr-enlarge-link">
                         <div class="fr-header__service">
-                            <p class="fr-header__service-title">Nous contacter</p>
+                            <p class="fr-header__service-title">Nous contacter: <i> piaf@data.gouv.fr</i></p>
                         </div>
                     </div>
                 </div>
