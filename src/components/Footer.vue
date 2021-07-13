0371cb5b8bcce1fac7cc0c2ec1b1c95ed3bbf3a3
Guillim <guillim@users.noreply.github.com>
2021-07-13 13:36:06 +0200

    adding a contact adress for questions (#39)

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <a :href="'mailto:piaf@data.gouv.fr'">
+      <header role="banner" class="fr-header">
+        <div class="fr-header__body">
+            <div class="fr-container">
+                <div class="fr-header__body-row justify-center">
+                    <div class="fr-header__brand fr-enlarge-link">
+                        <div class="fr-header__service">
+                            <p class="fr-header__service-title">Nous contacter</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </header>
+    </a>
+  </div>
+</template>
